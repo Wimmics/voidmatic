19ffba765e9db56fbd442c22ec7abc5efc40da30
Pierre Maillot <maillpierre@gmail.com>
2023-02-10 16:01:31 +0100

    typing functions

--- src/js/Validation.ts
+++ src/js/Validation.ts
@@ -1,7 +1,7 @@
 import * as $rdf from 'rdflib';
 import dayjs from 'dayjs';
 
-export function isLiteral(value) {
+export function isLiteral(value: string): boolean {
     try {
         return value != undefined && value.length > 0 && $rdf.isLiteral($rdf.lit(value));
     } catch (e) {
@@ -9,7 +9,7 @@ export function isLiteral(value) {
     }
 }
 
-export function isURI(value) {
+export function isURI(value: string): boolean {
     try {
         return value != undefined && value.length > 0 && $rdf.isNamedNode($rdf.sym(value));
     } catch (e) {
@@ -17,7 +17,7 @@ export function isURI(value) {
     }
 }
 
-export function isNotBlank(value) {
+export function isNotBlank(value: string): boolean {
     try {
         return isURI(value) || isLiteral(value);
     } catch (e) {
@@ -25,7 +25,7 @@ export function isNotBlank(value) {
     }
 }
 
-export function isDatetime(value) {
+export function isDatetime(value: string): boolean {
     try {
         return isLiteral(value) && dayjs(value).isValid();
     } catch (e) {
@@ -33,15 +33,15 @@ export function isDatetime(value) {
     }
 }
 
-export function isDuration(value) {
+export function isDuration(value: string): boolean {
     try {
-        return isLiteral(value) && dayjs(inputVal).isValid() && dayjs.isDuration(dayjs(inputVal));
+        return isLiteral(value) && dayjs(value).isValid() && dayjs.isDuration(dayjs(value));
     } catch (e) {
         return false;
     }
 }
 
-export function isInteger(value) {
+export function isInteger(value: string): boolean {
     try {
         return isLiteral(value) && Number.isInteger(Number.parseInt(value));
     } catch (e) {
@@ -49,7 +49,7 @@ export function isInteger(value) {
     }
 }
 
-export function isPositiveInteger(value) {
+export function isPositiveInteger(value: string): boolean {
     try {
         return isInteger(value) && (Number.parseInt(value) > 0);
     } catch (e) {
